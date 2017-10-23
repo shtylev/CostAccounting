13107e9fe285a0246c7b876b755f1b9c22d490f0
shtylev <shtylevAA@rambler.ru>
2017-10-23 20:30:43 +0300

    Добавление класса сущности

---
+++ CostAccounting/DAL/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CostAccounting.Model_Data;
+
+namespace CostAccounting.DAL
+{
+    public static class Config
+    {
+        public static DbCostAccountingEntities db = new DbCostAccountingEntities();
+    }
+}
