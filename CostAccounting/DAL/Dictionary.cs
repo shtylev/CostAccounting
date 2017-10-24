584e72534c72c2451103af455b51cda8edb6987f
shtylev <shtylevAA@rambler.ru>
2017-10-24 23:19:19 +0300

    Добавление класса

---
+++ CostAccounting/DAL/Dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostAccounting.DAL
+{
+    public class Dictionary
+    {
+        public enum TypeReference
+        {
+            Analytics = 1,
+            Articles = 2
+        }
+    }
+}
