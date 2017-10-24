7f664f294b7ec584a91edcdadfb5a648d200d71a
shtylev <shtylevAA@rambler.ru>
2017-10-24 23:18:04 +0300

    Добавление метода получения статей

--- CostAccounting/DAL/Articles.cs
+++ CostAccounting/DAL/Articles.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CostAccounting.Model_Data;
 
 namespace CostAccounting.DAL
 {
-    public class Articles
+    public static class Articles
     {
+        public static List<Model_Data.Articles> GetArticles()
+        {
+            return Config.db.Articles.Where(a => a.Active == true).OrderBy(n => n.Name).ToList();
+        }
     }
 }
