bb8537d5019f896c44dfdb0bdb74e5dcea906ea5
shtylev <shtylevAA@rambler.ru>
2017-10-24 15:51:31 +0300

    Добавление метода, для получения аналитик

--- CostAccounting/DAL/Analytics.cs
+++ CostAccounting/DAL/Analytics.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CostAccounting.Model_Data;
 
 namespace CostAccounting.DAL
 {
-    public class Analytics
+    public static class Analytics
     {
+        public static List<Model_Data.Analytics> GetAnalitics()
+        {
+            return Config.db.Analytics.OrderBy(n => n.Name).ToList();
+        }
     }
 }
