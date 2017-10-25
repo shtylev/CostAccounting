9e44c0052f266942f3db677fba0f7d3cf8f446dd
shtylev <shtylevAA@rambler.ru>
2017-10-25 20:13:07 +0300

    Переименование класса

---
+++ CostAccounting/DAL/ArticlesModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CostAccounting.Model_Data;
+
+namespace CostAccounting.DAL
+{
+    public static class ArticlesModel
+    {
+        /// <summary>
+        /// Получение всех активных статей
+        /// </summary>
+        /// <returns></returns>
+        public static List<Articles> GetArticles()
+        {
+            return Config.db.Articles.Where(a => a.Active == true).OrderBy(n => n.Name).ToList();
+        }
+        /// <summary>
+        /// Создание статьи
+        /// </summary>
+        /// <param name="nameArticle">Имя новой статьи</param>
+        /// <returns></returns>
+        public static string CreateArticle(string nameArticle)
+        {
+            Articles newArticle = new Articles();
+            newArticle.Name = nameArticle;
+            newArticle.Active = true;
+            try
+            {
+                Config.db.Articles.Add(newArticle);
+                Config.db.SaveChanges();
+                return Resources.OK;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
