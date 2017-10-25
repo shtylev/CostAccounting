50b2e2ec96ef4bed973bbaf5d24866665b9bdf2a
shtylev <shtylevAA@rambler.ru>
2017-10-25 20:15:24 +0300

    Добавление формы, для добавления справочника

---
+++ CostAccounting/Forms/Menu/FormCreateRef.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CostAccounting.Forms.Menu
+{
+    partial class FormCreateRef
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNameRef = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите название";
+            // 
+            // txtNameRef
+            // 
+            this.txtNameRef.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtNameRef.Location = new System.Drawing.Point(15, 29);
+            this.txtNameRef.Name = "txtNameRef";
+            this.txtNameRef.Size = new System.Drawing.Size(253, 23);
+            this.txtNameRef.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(56, 67);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(153, 67);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FormCreateRef
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 99);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtNameRef);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormCreateRef";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FormCreateRef_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNameRef;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ CostAccounting/Forms/Menu/FormCreateRef.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CostAccounting.DAL;
+
+namespace CostAccounting.Forms.Menu
+{
+    public partial class FormCreateRef : Form
+    {
+        private FormReferences formRef;
+        public FormCreateRef(FormReferences formRef)
+        {
+            InitializeComponent();
+            this.formRef = formRef;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            string result = "";
+
+            if (formRef.lblTypeReference.Text == Resources.Analytics)
+                result = AnalyticsModel.CreateAnalytic(txtNameRef.Text);
+            if (formRef.lblTypeReference.Text == Resources.Articles)
+                result = ArticlesModel.CreateArticle(txtNameRef.Text);
+
+            if (result == Resources.OK)
+            {
+                formRef.GetReference();
+                this.Close();
+            }
+            else
+                MessageBox.Show(result, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void FormCreateRef_Load(object sender, EventArgs e)
+        {
+            if (formRef.lblTypeReference.Text == Resources.Analytics)
+                this.Text = "Создание аналитики";
+            if (formRef.lblTypeReference.Text == Resources.Articles)
+                this.Text = "Создание статьи";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();            
+        }
+    }
+}
