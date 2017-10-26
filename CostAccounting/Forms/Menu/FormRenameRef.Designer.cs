bee631d1e13f42c142e83c221c4b99839e1b7d4f
shtylev <shtylevAA@rambler.ru>
2017-10-26 11:31:11 +0300

    Добавление и релаизация переименования справочников

---
+++ CostAccounting/Forms/Menu/FormRenameRef.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CostAccounting.Forms.Menu
+{
+    partial class FormRenameRef
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.txtNewNameRef = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(153, 67);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(56, 67);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // txtNewNameRef
+            // 
+            this.txtNewNameRef.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtNewNameRef.Location = new System.Drawing.Point(15, 29);
+            this.txtNewNameRef.Name = "txtNewNameRef";
+            this.txtNewNameRef.Size = new System.Drawing.Size(253, 23);
+            this.txtNewNameRef.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите новое название";
+            // 
+            // FormRenameRef
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 99);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtNewNameRef);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormRenameRef";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FormRenameRef_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox txtNewNameRef;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CostAccounting/Forms/Menu/FormRenameRef.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CostAccounting.DAL;
+
+namespace CostAccounting.Forms.Menu
+{
+    public partial class FormRenameRef : Form
+    {
+        FormReferences formRef;
+        public FormRenameRef(FormReferences formRef)
+        {
+            InitializeComponent();
+            this.formRef = formRef;
+        }
+
+        private void FormRenameRef_Load(object sender, EventArgs e)
+        {
+            if (formRef.lblTypeReference.Text == Resources.Analytics)
+                this.Text = "Переименование аналитики";
+            if(formRef.lblTypeReference.Text == Resources.Articles)
+                this.Text = "Переименование статьи";
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (txtNewNameRef.Text != "")
+            {
+                string result = "";
+
+                //получаем id выбранного справочника
+                int idRef = (int)formRef.listBoxReference.SelectedValue;
+
+                if (formRef.lblTypeReference.Text == Resources.Analytics)
+                    result = AnalyticsModel.RenameAnalytic(txtNewNameRef.Text, idRef);
+                if (formRef.lblTypeReference.Text == Resources.Articles)
+                    result = ArticlesModel.RenameArticle(txtNewNameRef.Text, idRef);
+
+                if (result == Resources.OK)
+                {
+                    formRef.GetReference();
+                    this.Close();
+                }                    
+                else
+                    MessageBox.Show(result, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
