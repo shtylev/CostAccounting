e540aacb8fc712db96586d769e6ac0cd12994e30
shtylev <shtylevAA@rambler.ru>
2017-10-26 11:29:56 +0300

    Добавление методов получения одной аналитики и переименование аналитики

--- CostAccounting/DAL/AnalyticsModel.cs
+++ CostAccounting/DAL/AnalyticsModel.cs
@@ -18,6 +18,15 @@ namespace CostAccounting.DAL
             return Config.db.Analytics.Where(a => a.Active == true).OrderBy(n => n.Name).ToList();
         }
         /// <summary>
+        /// Получение одной аналитики
+        /// </summary>
+        /// <param name="idAnalytic">id аналитики</param>
+        /// <returns></returns>
+        public static Analytics GetAnalytic(int idAnalytic)
+        {
+            return Config.db.Analytics.Where(i => i.Id == idAnalytic).FirstOrDefault();
+        }
+        /// <summary>
         /// Создание аналитики
         /// </summary>
         /// <param name="nameAnalytic">Имя новой аналитики</param>
@@ -37,6 +46,26 @@ namespace CostAccounting.DAL
             {
                 return ex.Message;
             }            
+        }
+        /// <summary>
+        /// Переименование выбранной аналитики
+        /// </summary>
+        /// <param name="newName">Новое имя аналитики</param>
+        /// <param name="idAnalytic">id выбранной аналитики</param>
+        /// <returns></returns>
+        public static string RenameAnalytic(string newName, int idAnalytic)
+        {
+            Analytics analytic = GetAnalytic(idAnalytic);
+            analytic.Name = newName;
+            try
+            {
+                Config.db.SaveChanges();
+                return Resources.OK;
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
