1b1afb15ffd49fbb2da77be0c2126e31bbb71ca5
shtylev <shtylevAA@rambler.ru>
2017-10-26 11:30:14 +0300

    Добавление методов получения одной статьи и переименование статьи

--- CostAccounting/DAL/ArticlesModel.cs
+++ CostAccounting/DAL/ArticlesModel.cs
@@ -18,6 +18,16 @@ namespace CostAccounting.DAL
             return Config.db.Articles.Where(a => a.Active == true).OrderBy(n => n.Name).ToList();
         }
         /// <summary>
+        /// Получение статьи
+        /// </summary>
+        /// <param name="idArticle">id статьи</param>
+        /// <returns></returns>
+        public static Articles GetArticle(int idArticle)
+        {
+            return Config.db.Articles.Where(i => i.Id == idArticle).FirstOrDefault();
+        }
+
+        /// <summary>
         /// Создание статьи
         /// </summary>
         /// <param name="nameArticle">Имя новой статьи</param>
@@ -34,6 +44,26 @@ namespace CostAccounting.DAL
                 return Resources.OK;
             }
             catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        /// <summary>
+        /// Переименование выбранной статьи
+        /// </summary>
+        /// <param name="newName">Новое имя статьи</param>
+        /// <param name="idArticle">id выбранной статьи</param>
+        /// <returns></returns>
+        public static string RenameArticle(string newName, int idArticle)
+        {
+            Articles article = GetArticle(idArticle);
+            article.Name = newName;
+            try
+            {
+                Config.db.SaveChanges();
+                return Resources.OK;
+            }
+            catch(Exception ex)
             {
                 return ex.Message;
             }
