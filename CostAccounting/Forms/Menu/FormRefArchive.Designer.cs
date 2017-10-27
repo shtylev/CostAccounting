c05758c8d629941e93f6c4a0ab334ecfda1f67a1
shtylev <shtylevAA@rambler.ru>
2017-10-27 13:27:21 +0300

    Добавление формы просмотра архивных справочников

---
+++ CostAccounting/Forms/Menu/FormRefArchive.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace CostAccounting.Forms.Menu
+{
+    partial class FormRefArchive
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxRefArchive = new System.Windows.Forms.ListBox();
+            this.cmsRefArchive = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsRefArchive.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxRefArchive
+            // 
+            this.listBoxRefArchive.ContextMenuStrip = this.cmsRefArchive;
+            this.listBoxRefArchive.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxRefArchive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxRefArchive.FormattingEnabled = true;
+            this.listBoxRefArchive.ItemHeight = 16;
+            this.listBoxRefArchive.Location = new System.Drawing.Point(0, 0);
+            this.listBoxRefArchive.Name = "listBoxRefArchive";
+            this.listBoxRefArchive.Size = new System.Drawing.Size(154, 201);
+            this.listBoxRefArchive.TabIndex = 0;
+            // 
+            // cmsRefArchive
+            // 
+            this.cmsRefArchive.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.cmsRefArchive.Name = "contextMenuStrip1";
+            this.cmsRefArchive.Size = new System.Drawing.Size(174, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(173, 22);
+            this.toolStripMenuItem1.Text = "Удалить из архива";
+            // 
+            // FormRefArchive
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(154, 201);
+            this.Controls.Add(this.listBoxRefArchive);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormRefArchive";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FormRefArchive_Load);
+            this.cmsRefArchive.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxRefArchive;
+        private System.Windows.Forms.ContextMenuStrip cmsRefArchive;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
---
+++ CostAccounting/Forms/Menu/FormRefArchive.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CostAccounting.DAL;
+
+namespace CostAccounting.Forms.Menu
+{
+    public partial class FormRefArchive : Form
+    {
+        private FormReferences formRef;
+        public FormRefArchive(FormReferences formRef)
+        {
+            InitializeComponent();
+            this.formRef = formRef;
+        }
+
+        private void FormRefArchive_Load(object sender, EventArgs e)
+        {
+            GetRefArchive();
+        }
+        private void GetRefArchive()
+        {
+            if (formRef.lblTypeReference.Text == Resources.Analytics)
+            {
+                this.Text = "Архив аналитик";
+                listBoxRefArchive.DataSource = AnalyticsModel.GerAnalyticsArchive();
+            }
+            if (formRef.lblTypeReference.Text == Resources.Articles)
+            {
+                this.Text = "Архив статей";
+                listBoxRefArchive.DataSource = ArticlesModel.GetArticlesArchive();
+            }
+
+            listBoxRefArchive.DisplayMember = "Name";
+            listBoxRefArchive.ValueMember = "Id";
+        }
+    }
+}
