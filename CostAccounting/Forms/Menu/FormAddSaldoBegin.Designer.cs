89b184b1bfb4065ec8053fe2031a82b01ac54a86
shtylev <shtylevAA@rambler.ru>
2017-11-13 20:45:41 +0300

    Добавление новой формы, для ввода начального сальдо

---
+++ CostAccounting/Forms/Menu/FormAddSaldoBegin.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace CostAccounting.Forms.Menu
+{
+    partial class FormAddSaldoBegin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormAddSaldoBegin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 407);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormAddSaldoBegin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Сальдо на начало периода";
+            this.Load += new System.EventHandler(this.FormAddSaldoBegin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
