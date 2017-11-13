7df2f99d852a48ec0fca891ff7282cebe1834256
shtylev <shtylevAA@rambler.ru>
2017-11-13 20:45:02 +0300

    Добавление класса, для словаря

---
+++ CostAccounting/DAL/Dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostAccounting.DAL
+{
+    public class Dictionary
+    {
+        enum TypeSaldo
+        {
+            saldoStartingPeriod = 1,
+            saldoEndPeriod = 2
+        }
+    }
+}
