db77ea6a0be93f85e7c370a3af7555c6fd0fa548
shtylev <shtylevAA@rambler.ru>
2017-11-14 13:21:07 +0300

    Добавление класса, для модели сальдо

---
+++ CostAccounting/Model/SaldoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostAccounting.Model
+{
+    public class SaldoModel
+    {
+        public string Name { get; set; }
+        public double Sum { get; set; }
+    }
+}
