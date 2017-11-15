bbbeaea6c82cf7135f4c6bec629c1fa2bbe372f8
shtylev <shtylevAA@rambler.ru>
2017-11-15 20:27:36 +0300

    Добавление класса SaldoEntities и метода, для получения начального сальдо

---
+++ CostAccounting/DAL/SaldoEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CostAccounting.Model_Data;
+
+namespace CostAccounting.DAL
+{
+    static public class SaldoEntities
+    {
+        public static List<Saldo> GetSaldosStartingPeriod()
+        {
+            return Config.db.Saldo.Where(t => t.Type == (int)Dictionary.TypeSaldo.saldoStartingPeriod).ToList();
+        }
+    }
+}
