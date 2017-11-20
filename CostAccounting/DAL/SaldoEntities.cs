cf0be5a9d585318adbdbca2e35a8dad6b89942c0
shtylev <shtylevAA@rambler.ru>
2017-11-20 20:51:18 +0300

    Добавление метода поиска аналитики или статьи

--- CostAccounting/DAL/SaldoEntities.cs
+++ CostAccounting/DAL/SaldoEntities.cs
@@ -9,9 +9,41 @@ namespace CostAccounting.DAL
 {
     static public class SaldoEntities
     {
+        /// <summary>
+        /// Получает список сальдо на начало периода
+        /// </summary>
+        /// <returns></returns>
         public static List<Saldo> GetSaldosStartingPeriod()
         {
             return Config.db.Saldo.Where(t => t.Type == (int)Dictionary.TypeSaldo.saldoStartingPeriod).ToList();
+        }
+        /// <summary>
+        /// Выполняет поиск справочника в сальдо на начало периода
+        /// </summary>
+        /// <param name="idAnalytic">id аналитики</param>
+        /// <param name="idArticle">id статьи</param>
+        /// <returns></returns>
+        public static bool FindEntryRefInSaldoStartingPeriod(int? idAnalytic, int? idArticle)
+        {
+            Saldo saldo = new Saldo();
+
+            if (idAnalytic != null)
+            {
+                saldo = Config.db.Saldo.Where(i => i.IdAnalytic == idAnalytic).FirstOrDefault();
+
+                if (saldo != null)
+                    return true;
+            }
+
+            if (idArticle != null)
+            {
+                saldo = Config.db.Saldo.Where(i => i.IdArticle == idArticle).FirstOrDefault();
+
+                if (saldo != null)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
