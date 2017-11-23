e340d0b6ad0e7ac8796e9378f2cbb62fe3e16ac6
shtylev <shtylevAA@rambler.ru>
2017-11-23 19:02:51 +0300

    Добавление 3 свойств

--- CostAccounting/Model/SaldoModel.cs
+++ CostAccounting/Model/SaldoModel.cs
@@ -10,11 +10,17 @@ namespace CostAccounting.Model
     {
         public string Name { get; set; }
         public double Sum { get; set; }
+        public int? IdArticle { get; set; }
+        public int? IdAnalytic { get; set; }
+        public int Id { get; set; }
 
-        public SaldoModel(string name, double sum)
+        public SaldoModel(string name, double sum, int? idArticle, int? idAnalytic, int id)
         {
             Name = name;
             Sum = sum;
+            IdAnalytic = idAnalytic;
+            IdArticle = idArticle;
+            Id = id;
         }
     }
 }
