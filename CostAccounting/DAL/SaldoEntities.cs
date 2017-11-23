1f583116a1ccf8f83b84d34817a88dead14cff97
shtylev <shtylevAA@rambler.ru>
2017-11-23 19:02:35 +0300

    Добавление двух методов на поиск сальдо и удаление сальдо. Плюс добавление условия для поиска сальдо на начало периода

--- CostAccounting/DAL/SaldoEntities.cs
+++ CostAccounting/DAL/SaldoEntities.cs
@@ -23,13 +23,13 @@ namespace CostAccounting.DAL
         /// <param name="idAnalytic">id аналитики</param>
         /// <param name="idArticle">id статьи</param>
         /// <returns></returns>
-        public static bool FindEntryRefInSaldoStartingPeriod(int? idAnalytic, int? idArticle)
+        public static bool FindEntryRefInSaldoStartingPeriodById(int? idAnalytic, int? idArticle)
         {
             Saldo saldo = new Saldo();
 
             if (idAnalytic != null)
             {
-                saldo = Config.db.Saldo.Where(i => i.IdAnalytic == idAnalytic).FirstOrDefault();
+                saldo = Config.db.Saldo.Where(t => t.Type == (int)Dictionary.TypeSaldo.saldoStartingPeriod).Where(i => i.IdAnalytic == idAnalytic).FirstOrDefault();
 
                 if (saldo != null)
                     return true;
@@ -37,13 +37,48 @@ namespace CostAccounting.DAL
 
             if (idArticle != null)
             {
-                saldo = Config.db.Saldo.Where(i => i.IdArticle == idArticle).FirstOrDefault();
+                saldo = Config.db.Saldo.Where(t => t.Type == (int)Dictionary.TypeSaldo.saldoStartingPeriod).Where(i => i.IdArticle == idArticle).FirstOrDefault();
 
                 if (saldo != null)
                     return true;
             }
 
             return false;
+        }
+        /// <summary>
+        /// Поиск сальдо
+        /// </summary>
+        /// <param name="idSaldo">Id сальдо</param>
+        /// <returns></returns>
+        public static Saldo GetSaldoById(int idSaldo)
+        {
+            return Config.db.Saldo.Where(i => i.Id == idSaldo).FirstOrDefault();
+        } 
+        /// <summary>
+        /// Удаляет сальдо
+        /// </summary>
+        /// <param name="idSaldo"></param>
+        /// <returns></returns>
+        public static string DeleteSaldo(int idSaldo)
+        {
+            Saldo saldo = GetSaldoById(idSaldo);
+
+            if(saldo != null)
+            {
+                try
+                {
+                    Config.db.Saldo.Remove(saldo);
+                    Config.db.SaveChanges();
+
+                    return Resources.OK;
+                }
+                catch(Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+
+            return "Найти сальдо не удалось.";
         }
     }
 }
