808bb367dd0211856942943b8d09e0568ed270c8
shtylev <shtylevAA@rambler.ru>
2017-11-28 10:19:18 +0300

    незначительные изменения

--- CostAccounting/FormMain.Designer.cs
+++ CostAccounting/FormMain.Designer.cs
@@ -31,9 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
             this.menuMain = new System.Windows.Forms.MenuStrip();
             this.menuItemReference = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemAddSaldoBegin = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItemAnalyticsReference = new System.Windows.Forms.ToolStripMenuItem();
             this.menuItemArticlesReference = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuItemAddSaldoBegin = new System.Windows.Forms.ToolStripMenuItem();
             this.menuMain.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,7 +45,7 @@
             this.menuMain.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
             this.menuMain.Location = new System.Drawing.Point(0, 0);
             this.menuMain.Name = "menuMain";
-            this.menuMain.Size = new System.Drawing.Size(1142, 24);
+            this.menuMain.Size = new System.Drawing.Size(1398, 24);
             this.menuMain.TabIndex = 0;
             this.menuMain.Text = "menuStrip1";
             // 
@@ -59,32 +59,32 @@
             this.menuItemReference.Size = new System.Drawing.Size(94, 20);
             this.menuItemReference.Text = "Справочники";
             // 
+            // menuItemAddSaldoBegin
+            // 
+            this.menuItemAddSaldoBegin.Name = "menuItemAddSaldoBegin";
+            this.menuItemAddSaldoBegin.Size = new System.Drawing.Size(222, 22);
+            this.menuItemAddSaldoBegin.Text = "Сальдо на начало периода";
+            this.menuItemAddSaldoBegin.Click += new System.EventHandler(this.menuItemAddSaldoBegin_Click);
+            // 
             // menuItemAnalyticsReference
             // 
             this.menuItemAnalyticsReference.Name = "menuItemAnalyticsReference";
-            this.menuItemAnalyticsReference.Size = new System.Drawing.Size(214, 22);
+            this.menuItemAnalyticsReference.Size = new System.Drawing.Size(222, 22);
             this.menuItemAnalyticsReference.Text = "Аналитики";
             this.menuItemAnalyticsReference.Click += new System.EventHandler(this.menuItemAnalyticsReference_Click);
             // 
             // menuItemArticlesReference
             // 
             this.menuItemArticlesReference.Name = "menuItemArticlesReference";
-            this.menuItemArticlesReference.Size = new System.Drawing.Size(214, 22);
+            this.menuItemArticlesReference.Size = new System.Drawing.Size(222, 22);
             this.menuItemArticlesReference.Text = "Статьи";
             this.menuItemArticlesReference.Click += new System.EventHandler(this.menuItemArticlesReference_Click);
-            // 
-            // menuItemAddSaldoBegin
-            // 
-            this.menuItemAddSaldoBegin.Name = "menuItemAddSaldoBegin";
-            this.menuItemAddSaldoBegin.Size = new System.Drawing.Size(214, 22);
-            this.menuItemAddSaldoBegin.Text = "Ввести начальное сальдо";
-            this.menuItemAddSaldoBegin.Click += new System.EventHandler(this.menuItemAddSaldoBegin_Click);
             // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1142, 450);
+            this.ClientSize = new System.Drawing.Size(1398, 668);
             this.Controls.Add(this.menuMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
