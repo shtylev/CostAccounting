ee8771e66300fe40b8eed2830b6f40308ea14cc7
shtylev <shtylevAA@rambler.ru>
2017-12-01 20:49:45 +0300

    Добавление класса для Costs

---
+++ CostAccounting/DAL/CostsEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CostAccounting.Model_Data;
+
+namespace CostAccounting.DAL
+{
+    public static class CostsEntities
+    {
+        public static List<Costs> GetCosts()
+        {
+            return Config.db.Costs.ToList();
+        }
+    }
+}
