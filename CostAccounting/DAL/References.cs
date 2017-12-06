67bab84b45430915d02134102ff8431f4cfe9a63
shtylev <shtylevAA@rambler.ru>
2017-12-06 20:46:30 +0300

    Переименование класса

---
+++ CostAccounting/DAL/References.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostAccounting.DAL
+{
+    public class References
+    {
+        public enum TypeSaldo
+        {
+            startingPeriod = 1,         //сальдо на начало периода
+            endPeriod = 2,              //сальдо на конец периода
+            totalSumForEndPeriod = 3,   //общие суммы, для сальдо на конец периода
+        }
+    }
+}
