b120d195c12a1d3ed970992b20a179c6c5e046c6
shtylev <shtylevAA@rambler.ru>
2017-12-14 21:03:33 +0300

    Добавление нового метода, для получения расходов за месяц

--- CostAccounting/DAL/CostsEntities.cs
+++ CostAccounting/DAL/CostsEntities.cs
@@ -13,5 +13,16 @@ namespace CostAccounting.DAL
         {
             return Config.db.Costs.ToList();
         }
+        /// <summary>
+        /// Возвращает список расходов, по статье, месяцу и году
+        /// </summary>
+        /// <param name="idArticle"></param>
+        /// <param name="month"></param>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public static List<Costs> GetCostsForMonthArticle(int idArticle, int month, int year)
+        {
+            return Config.db.Costs.Where(i => i.IdArticle == idArticle).Where(dm => dm.Date.Month == month).Where(dy => dy.Date.Year == year).ToList();
+        }
     }
 }
