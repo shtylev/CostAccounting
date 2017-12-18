1555aa8b13aafd1ddb3ca41dd46d9eecc4ccdb48
shtylev <shtylevAA@rambler.ru>
2017-12-18 14:38:27 +0300

    Переименование лейблов

--- CostAccounting/FormMain.Designer.cs
+++ CostAccounting/FormMain.Designer.cs
@@ -138,9 +138,9 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(385, 32);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(110, 17);
+            this.label5.Size = new System.Drawing.Size(54, 17);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Введите сумму:";
+            this.label5.Text = "Сумма:";
             // 
             // txtSumCost
             // 
@@ -164,9 +164,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(643, 32);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(145, 17);
+            this.label4.Size = new System.Drawing.Size(102, 17);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Введите сообщение:";
+            this.label4.Text = "Комментарий:";
             // 
             // txtCostMessage
             // 
@@ -180,9 +180,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(513, 31);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.Size = new System.Drawing.Size(46, 17);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Выберите дату:";
+            this.label3.Text = "Дата:";
             // 
             // dtpCostDate
             // 
@@ -196,11 +196,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(195, 32);
+            this.label2.Location = new System.Drawing.Point(195, 31);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 17);
+            this.label2.Size = new System.Drawing.Size(58, 17);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Выберите статью:";
+            this.label2.Text = "Статья:";
             // 
             // cmbCostsArticles
             // 
@@ -215,9 +215,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(7, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.Size = new System.Drawing.Size(83, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Выберите аналитику:";
+            this.label1.Text = "Аналитика:";
             // 
             // cmbCostsAnalytics
             // 
