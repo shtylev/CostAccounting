353f33464bf98564b0b29931c28a50042ceb8266
shtylev <shtylevAA@rambler.ru>
2017-12-19 20:26:52 +0300

    Добавление класса, для таблицы расходов

---
+++ CostAccounting/Model/CostModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostAccounting.Model
+{
+    public class CostModel
+    {
+        public int Id { get; set; }
+        public string NameAnalytic { get; set; }
+        public string NameArticle { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public double Sum { get; set; }
+
+        public CostModel()
+        {
+
+        }
+        public CostModel(int id, string nameAnalytic, string nameArticle, DateTime date, double sum, string message)
+        {
+            Id = id;
+            NameAnalytic = nameAnalytic;
+            NameArticle = nameArticle;
+            Date = date;
+            Sum = sum;
+            Message = message;
+        }
+    }
+}
