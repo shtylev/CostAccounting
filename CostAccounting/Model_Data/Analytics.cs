5f27e28595e2f4550df539f530bcec62163632fe
shtylev <shtylevAA@rambler.ru>
2017-12-22 15:22:49 +0300

    Добавление нового столбца для цвета

--- CostAccounting/Model_Data/Analytics.cs
+++ CostAccounting/Model_Data/Analytics.cs
@@ -24,6 +24,7 @@ namespace CostAccounting.Model_Data
         public int Id { get; set; }
         public string Name { get; set; }
         public bool Active { get; set; }
+        public string Color { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Costs> Costs { get; set; }
