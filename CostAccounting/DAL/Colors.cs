22621e9c1bd5a372898530e611a98045db6e9743
shtylev <shtylevAA@rambler.ru>
2017-12-22 19:44:42 +0300

    Добавление нового класса, для цвета

---
+++ CostAccounting/DAL/Colors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CostAccounting.DAL
+{
+    public static class Colors
+    {
+        /// <summary>
+        /// Возвращает цвет из hex значения
+        /// </summary>
+        /// <param name="colorHex"></param>
+        /// <returns></returns>
+        public static Color GetColor(string colorHex)
+        {
+            ColorConverter conv = new ColorConverter();
+            return (Color)conv.ConvertFromString(colorHex);
+        }
+    }
+}
